3f69c2e3de797084c92f85d7edffa2a02da52a32
Faizan Luthyanvi <codingmonkey76@gmail.com>
2026-06-13 21:32:48 +0530

    commiting

--- frontend/index.py
+++ frontend/index.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "http://43.205.96.86:8000"
 
 st.set_page_config(page_title="Finzaari | Exclusive Jewelry", layout="wide")
 
